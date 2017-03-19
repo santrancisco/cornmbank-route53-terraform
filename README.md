cd01d871992c58ee05fd6b45b7180e86f383517e
san <san.tran@ebfe.pw>
2017-03-20 02:55:30 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# How to
+
+This is my terraform code to provision/config route53 for my cornmbank.com site. It's my playground to learn terraform. If it helps you, great.
+
+For this task, i created a new user in AWS with full access to Route53 policy attached.
+
+I also created a S3 bucket folder with Infrequent Usage, AES256 encryption to store the tfstate files and make it easier to manage route53 in different machines.
+
+Below is a custom IAM policy you can attach to your custom group for any account doing Route53 changes. This policy let user has full access to particular S3 bucket folder which contains the tfstate file.
+
+To verify this is working, using the aws-cli with the specified user credential, run this aws command: `aws s3 ls s3://OUR-S3-BUCKET-NAME/PATH-TO/route53/`. 
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowAllS3ActionsInRoute53Folder",
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::YOUR-S3-BUCKET-NAME/PATH-TO/route53/*"
+            ]
+        }
+    ]
+}
+```
+
+
+Create a config.tf file with the following content
+```
+terraform {
+  backend "s3" {
+    bucket = "YOUR-S3-BUCKET-NAME"
+    key    = "PATH-TO/route53/"
+    region = "REGION-OF-BUCKET"
+  }
+}
+
+```
+
+To finalise the configuration and make sure terraform will use s3 bucket for its state, run `terraform init`. you should now see a .terraform folder being created. 
+NOTE: You may need working AWS-cli installed with accesskey and secret for this user account configured. I have not tested it without this credential pre-set.
---
+++ cornmbank.tf
@@ -0,0 +1,117 @@
+##
+## The following lines declare all variables and their default values
+## If terraform.tfvars exist, it will be loaded and overwrite any of these variable
+## To load a seperate tfvar file (you may have a secret.tfvars file, use -var-file="secret.tfvars" switch)
+variable "access-key" {}
+variable "secret-key" {}
+variable "bucket-name" {
+  default="YOUR-BUCKET-NAME"
+}
+variable "bucket-key" {
+  default="PATH/route53/"
+}
+variable "bucket-region" {
+  default="ap-southeast-2"
+}
+
+data "terraform_remote_state" "state" {
+    backend = "s3"
+    config {
+        bucket = "${var.bucket-name}"
+        key = "${var.bucket-key}/terraform.tfstate"
+        region = "${var.bucket-region}"
+    }
+}
+
+provider "aws" {
+  access_key = "${var.access-key}"
+  secret_key = "${var.secret-key}"
+  region     = "us-east-1"
+}
+
+variable "bodhi-address" {
+  type = "string"
+  default = "www.bodhiandpriya.com"
+}
+
+variable "san-machine" {
+  type = "string"
+  default = "107.6.143.150"
+}
+
+# Define our primary zone
+
+resource "aws_route53_zone" "cornmbank-main" {
+  name    = "cornmbank.com"
+  comment = "San's playground"
+}
+
+# Define a subdomain zone - Usually at this step in console, we will note down the NS servers
+# for the subdomain to help with next step
+
+resource "aws_route53_zone" "cornmbank-dev" {
+  name = "dev.cornmbank.com"
+  comment = "San's playground 2"
+  tags {
+    Enabled = "false"
+  }
+}
+
+# This is how we can create NS record for our subdomain zone in the primary zone
+# (Notice the record is create in cornmbank-main & the 4 records are from dev zone.)
+# Usually in the console, this involve copy-pasta these NS server addresses.
+
+resource "aws_route53_record" "cornmbank-dev-ns" {
+  zone_id = "${aws_route53_zone.cornmbank-main.zone_id}"
+  name    = "dev.cornmbank.com"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.cornmbank-dev.name_servers.0}",
+    "${aws_route53_zone.cornmbank-dev.name_servers.1}",
+    "${aws_route53_zone.cornmbank-dev.name_servers.2}",
+    "${aws_route53_zone.cornmbank-dev.name_servers.3}",
+  ]
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = "${aws_route53_zone.cornmbank-main.zone_id}"
+  name    = "cornmbank.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.san-machine}"]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.cornmbank-main.zone_id}"
+  name    = "www.cornmbank.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${var.san-machine}"]
+}
+
+resource "aws_route53_record" "txt" {
+  zone_id = "${aws_route53_zone.cornmbank-main.zone_id}"
+  name    = "cornmbank.com"
+  type    = "TXT"
+  ttl     = "300"
+  records = ["Hello Word"]
+}
+
+resource "aws_route53_record" "bodhi" {
+  zone_id = "${aws_route53_zone.cornmbank-main.zone_id}"
+  name    = "bodhi.cornmbank.com."
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${var.bodhi-address}"]
+  provisioner "local-exec" {
+    command = "echo This provisioner script runs only at creation of bodhi CNAME record =)."
+    on_failure = "continue"
+  }
+  provisioner "local-exec" {
+    command = "echo And This provisioner script runs at destruction of bodhi CNAME record =)."
+    on_failure = "continue"
+    when = "destroy"
+  }
+}
